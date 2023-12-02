1095f93eb521509e4513fe9d0466f1fdbdea03a9
Mina-Wasfy <124878303+Mina-Wasfy@users.noreply.github.com>
2023-12-02 13:44:22 +0200

    SeasonOfTheYear.java

--- SeasonOfTheYear.java
+++ SeasonOfTheYear.java
@@ -8,15 +8,12 @@ public class SeasonOfTheYear {
     public static void main(String arg[]){
         Scanner scanner = new Scanner(System.in);
 
-        //1-Ask a user for a season of the year
         System.out.println("Enter The season of the year:");
         String season = scanner.next();
 
-        //2-Whole number of the season
         System.out.println("Enter the whole number");
         int number = scanner.nextInt();
 
-        //3-Ask about adjective
         System.out.println("Enter an Adjective");
         String adjective = scanner.next();
 
